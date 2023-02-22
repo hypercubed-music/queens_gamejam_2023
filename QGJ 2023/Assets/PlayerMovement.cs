c0130890d35ab30f60ef055c393f1c457dde691d
Jayden Ting <19tkt1@queensu.ca>
2023-02-21 21:47:58 -0500

    Left/Right movement script added

---
+++ QGJ 2023/Assets/PlayerMovement.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 1.5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 pos = transform.position;
+        
+        if (Input.GetKey ("d") || Input.GetKey(KeyCode.RightArrow)) {
+            pos.x += speed * Time.deltaTime;
+        }
+        if (Input.GetKey ("a") || Input.GetKey(KeyCode.LeftArrow)) {
+            pos.x -= speed * Time.deltaTime;
+        }
+ 
+        transform.position = pos;
+    }
+}
